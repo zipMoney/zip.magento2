d35a4822744bee987f1bd184f3762eb98b9fcb84
mahbub-zip <mahbubur.rahman@zip.co>
2020-08-26 19:59:19 +1000

    send error data to checkout js

--- view/frontend/web/js/action/place-zip-order.js
+++ view/frontend/web/js/action/place-zip-order.js
@@ -56,6 +56,7 @@ define(
             });
           }).fail(function (data) {
               errorProcessor.process(data, messageContainer);
+              reject(data.responseJSON);
               fullScreenLoader.stopLoader();
           }).always(function (data) {
             fullScreenLoader.stopLoader();
