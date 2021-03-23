56f74be243b440916bb3fac29a890d43e39c32dd
ZM\Mahbubur.Rahman <mahbubur.rahman@zip.co>
2021-03-23 16:57:54 +1100

    changed the if condition for checkout id

--- Controller/Standard/AbstractStandard.php
+++ Controller/Standard/AbstractStandard.php
@@ -242,14 +242,9 @@ abstract class AbstractStandard extends Action
         if (!$sessionQuote) {
             $this->_logger->error(__("Session Quote does not exist."));
             $use_checkout_api_quote = true;
-        } elseif ($checkout_id != $zipMoneyCheckoutId) {
-            // iframe redirect does not have au prefix for AU stack
-            if ($checkout_id != 'au-'.$zipMoneyCheckoutId) {
-                $this->_logger->error(__("Checkout Id does not match with the session quote."));
-                $use_checkout_api_quote = true;
-            } else {
-                return $sessionQuote;
-            }
+        } elseif ($checkout_id != $zipMoneyCheckoutId && $checkout_id != 'au-'.$zipMoneyCheckoutId) {
+            $this->_logger->error(__("Checkout Id does not match with the session quote."));
+            $use_checkout_api_quote = true;
         } else {
             return $sessionQuote;
         }
