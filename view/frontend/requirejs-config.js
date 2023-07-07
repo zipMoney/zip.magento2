b2bed7794d88e6dc7fcbaf8c55b0cbe2cef31fb8
Andre Susanto <andre.susanto@zip.co>
2023-07-07 11:13:43 +1000

    Update widget script url

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,6 @@
 var config = {
     paths: {
-        zipMoneyWidgetJs: 'https://static.zipmoney.com.au/lib/js/zm-widget-js/dist/zip-widget.min',
+        zipMoneyWidgetJs: 'https://static.zip.co/lib/js/zm-widget-js/dist/zip-widget.min',
         zipMoneyCheckoutJs: 'https://static.zipmoney.com.au/checkout/checkout-v1.min'
     }
 };
